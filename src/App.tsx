d744298e37d60d1fb958fd22ea53ba54807728cf
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-18 11:17:15 +0530

    changes on the width size and header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 export default function App() {
-  return <div>
+  return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
     <Header></ Header>
   </div>
 }
@@ -12,6 +12,8 @@ function Header() {
       <h1 className="text-3xl font-bold">Habit Tracker</h1>
       <span className="text-zinc-400 text-sm" >1 / 1 done today</span>
     </div>
-    <div></div>
+    <div className="flex flex-col gap-1">
+      <span className="text-zinc-400 text-sm">Apr 6 - Apr 12</span>
+    </div>
   </header>
 }
