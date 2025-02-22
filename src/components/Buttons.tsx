69a4da2a60b26186999cd457891c794c7f64ebff
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-22 21:02:10 +0530

    modifying the button component for prop and variants

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,9 +1,25 @@
-import type { ReactNode } from "react"
+import type { ComponentProps } from "react"
+
+type Variant = "primary" | "secondary" | "ghost-destructive"
 
 type ButtonProps = {
-    children: ReactNode
+    variant?: "primary" | "secondary" | "ghost-destructive"
+} & ComponentProps<"button">
+
+export function Button({ variant = "primary", ...props }: ButtonProps) {
+    return <button {...props} className="bg-blue-700 hover:bg-blue-600 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed" />
 }
 
-export function Button({ children }: ButtonProps) {
-    return <button className="bg-blue-700 hover:bg-blue-600 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>
+function getVariant(variant: Variant) {
+
+    switch (variant) {
+        case "primary":
+            return "bg-blue-700 hover:bg-blue-600"
+        case "secondary":
+            return "bg-zinc-700 hover:bg-zinc-600 text-zinc-400"
+        case "ghost-destructive":
+            return "hover:bg-red-600 text-red-800 hover:text-red-200"
+        default:
+            throw new Error(`Invalid Variant: ${variant satisfies never}`)
+    }
 }
