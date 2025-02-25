638d7e1ab50cb5b45f0af0a1a7c195b9312cc3b5
Himanish kochhar <himanish.kochhar@gmail.com>
2025-02-26 04:51:50 +0530

    created the add habit and habits state to use in list and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Header } from "./components/Header"
 import { HabitForm } from "./components/HabitForm"
-import { HabitList } from "./components/HabitList"
+import { HabitList, type Habit } from "./components/HabitList"
+import { useState } from "react"
 
 export default function App() {
+  const [habits, setHabits] = useState<Habit[]>([])
+
+  function addHabit(name: string) {
+    setHabits(currhabit => [...currhabit, { id: crypto.randomUUID(), name }])
+  }
+
   return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
     <Header />
-    <HabitForm />
-    <HabitList />
+    <HabitForm addHabit={addHabit} />
+    <HabitList habits={habits} />
   </div>
 }
 
