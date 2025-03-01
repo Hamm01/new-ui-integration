fbf3306d375a72b8423b73147c5f423c1182e58b
Himanish kochhar <himanish.kochhar@gmail.com>
2025-03-01 08:27:44 +0530

    Added the context provider in the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Header } from "./components/Header"
 import { HabitForm } from "./components/HabitForm"
-import { HabitList, type Habit } from "./components/HabitList"
+import { HabitList } from "./components/HabitList"
 import { useState } from "react"
 import { isSameDay } from "date-fns"
+import { HabitProvider, type Habit } from "./context/HabitProvider"
 
 export default function App() {
   const [habits, setHabits] = useState<Habit[]>([])
@@ -28,9 +29,11 @@ export default function App() {
   }
 
   return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
-    <Header />
-    <HabitForm addHabit={addHabit} />
-    <HabitList habits={habits} toggleHabit={toggleHabit} deleteHabit={deleteHabit} />
+    <HabitProvider>
+      <Header />
+      <HabitForm addHabit={addHabit} />
+      <HabitList habits={habits} toggleHabit={toggleHabit} deleteHabit={deleteHabit} />
+    </HabitProvider>
   </div>
 }
 
